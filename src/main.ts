89e9a9918ad93c3f07bf65fa81deaceab4c18075
kiyoung92 <go3go1go2@gmail.com>
2023-12-01 03:27:50 +0900

    test: jenkins pm2 test

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 
-  if (module.hot ) {
+  if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
   }
