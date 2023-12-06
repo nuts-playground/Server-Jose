1f8f39ada7b1cab1b5df084f144050af3dfc2720
kiyoung92 <go3go1go2@gmail.com>
2023-12-06 20:16:52 +0900

    test: Logger

--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -1,20 +1,20 @@
 import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
-const requestIp = require('request-ip')
+import { getClientIp } from 'request-ip';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   private logger = new Logger('HTTP');
 
   use(request: Request, response: Response, next: NextFunction): void {
-    const { ip, method, originalUrl } = request;
+    const { method, originalUrl } = request;
     const userAgent = request.get('user-agent') || '';
-    console.log(requestIp.getClientIp(request))
+    console.log(getClientIp(request))
     response.on('finish', () => {
       const { statusCode } = response;
       const contentLength = response.get('content-length');
       this.logger.log(
-        `[METHOD: ${method}] [ORIGIN_URL: ${originalUrl === undefined ? decodeURIComponent(originalUrl) : '/'}] [STATUS_CODE: ${statusCode}] [CONTENT_LENGTH: ${contentLength}] ::: [AGENT: ${userAgent}] [IP: ${requestIp.getClientIp(request)}]`,
+        `[METHOD: ${method}] [ORIGIN_URL: ${originalUrl === undefined ? decodeURIComponent(originalUrl) : '/'}] [STATUS_CODE: ${statusCode}] [CONTENT_LENGTH: ${contentLength}] ::: [AGENT: ${userAgent}] [IP: ${getClientIp(request)}]`,
       );
     });
 
