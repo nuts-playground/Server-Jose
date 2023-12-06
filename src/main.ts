33c8b0782e33d6388a10085b7f23bc580c853046
kiyoung92 <go3go1go2@gmail.com>
2023-12-06 19:57:09 +0900

    test: Logger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.set('trust proxy', true)
+  await app.listen(3000, '0.0.0.0');
 
   if (module.hot) {
     module.hot.accept();
--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -8,7 +8,7 @@ export class LoggerMiddleware implements NestMiddleware {
   use(request: Request, response: Response, next: NextFunction): void {
     const { ip, method, originalUrl } = request;
     const userAgent = request.get('user-agent') || '';
-    
+
     response.on('finish', () => {
       const { statusCode } = response;
       const contentLength = response.get('content-length');
