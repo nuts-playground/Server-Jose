27be4df32366caa311fa1b802e35950ae50b14ac
kiyoung92 <go3go1go2@gmail.com>
2023-12-10 03:13:36 +0900

    test: jenkins trigger

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'server port test';
+    return 'jenkins trigger test';
   }
 }
