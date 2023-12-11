e97ae39d6f68e6d5b470e2da7827e0979fb46119
kiyoung92 <go3go1go2@gmail.com>
2023-12-11 16:26:20 +0900

    test: connect prisma

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    console.log(`Connect MySQL!`);
+    console.log(`Connect MySQL!!`);
 
     await this.$connect();
   }
