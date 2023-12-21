c375462575fe1aa118583d2bbf873c265a5801c8
kiyoung92 <go3go1go2@gmail.com>
2023-12-21 22:41:26 +0900

    feat: redie for verification

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,28 +6,56 @@ import { ResponseDto } from 'src/common/dtos/response.dto';
 import { UserServiceUtil } from './utils/user.service.util';
 import { uuid_v4_generate } from 'src/common/utils/uuid.util';
 import { bcrypt_hash } from 'src/common/utils/bcrypt.util';
+import { RedisService } from 'src/redis/redis.service';
 
 @Injectable()
 export class UserService extends UserServiceUtil {
-  constructor(prisma: PrismaService) {
+  constructor(
+    prisma: PrismaService,
+    private readonly redisService: RedisService,
+  ) {
     super(prisma);
   }
 
-  async signUp(dto: SignUpDto): Promise<any> {
+  async signUp(dto: SignUpDto): Promise<ResponseDto> {
     const isAlreadyUser = await this.findOne(dto.getEmail());
 
     if (isAlreadyUser) {
       throw new UnauthorizedException('이미 가입된 메일입니다.');
     }
 
-    const hashedPassword: string = await bcrypt_hash(dto.getPassword());
+    const hashedPassword = await bcrypt_hash(dto.getPassword());
     await this.saveUser({
       id: uuid_v4_generate(),
       password: hashedPassword,
       ...dto.getSignUpInfo(),
     });
 
-    return ResponseDto.successWithJSON(dto.getSignUpInfo());
+    return ResponseDto.successWithJSON(dto.responseUserInfo());
+  }
+
+  // Need Create Controller
+  async sendVerificationEmail(email: string): Promise<any> {
+    const redis = this.redisService.getRedisClient();
+    const code = Math.floor(100000 + Math.random() * 900000).toString();
+
+    await redis.set(email, code, 'EX', 60 * 5);
+
+    return;
+  }
+
+  // Need Create Controller
+  async verifyEmail(email: string, verificationsCode: string): Promise<any> {
+    const redis = this.redisService.getRedisClient();
+    const storedCode = await redis.get(email);
+
+    if (storedCode !== verificationsCode) {
+      throw new UnauthorizedException('인증번호가 일치하지 않습니다.');
+    } else {
+      await redis.del(email);
+    }
+
+    return;
   }
 
   async signIn(dto: SignInDto): Promise<any> {
