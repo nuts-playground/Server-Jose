b30e4c0862f6afcdc299e94f3d929604f5af800d
kiyoung92 <go3go1go2@gmail.com>
2024-01-02 00:28:01 +0900

    comment: add todo

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalFilters(new AllExceptionsFilter());
   // TODO: setting cors
-  await app.listen(3001);
+  await app.listen(3000);
 }
 bootstrap();
