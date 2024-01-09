927fd502dfd354d57044e82314d5b61bba7bfd80
kiyoung92 <go3go1go2@gmail.com>
2024-01-09 23:23:18 +0900

    refactor: sign up dto

--- src/user/interface/sign-up.interface.ts
+++ src/user/interface/sign-up.interface.ts
@@ -1,28 +1,4 @@
-export interface SignUpInterface {
-  id: string;
-  email: string;
-  name: string;
-  password: string;
-  about_me?: string;
-  profile_image_url?: string;
-  delete_yn?: string;
-  created_at?: string;
-  updated_at?: string;
-}
-
 export interface SignUpResponseInterface {
   email: string;
   name: string;
-}
-
-export interface SignUpServiceResponseInterface {
-  id: string;
-  email: string;
-  name: string;
-  password: string;
-  about_me?: string;
-  profile_image_url?: string;
-  created_at?: Date;
-  updated_at?: Date;
-  delete_yn?: string;
 }
