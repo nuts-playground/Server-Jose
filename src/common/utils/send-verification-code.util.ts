fb260f6211a7329197ccb9bf814629178529e987
kiyoung92 <go3go1go2@gmail.com>
2024-01-11 01:50:03 +0900

    refactor: change util function
    change to functional

--- src/common/utils/send-verification-code.util.ts
+++ src/common/utils/send-verification-code.util.ts
@@ -3,17 +3,22 @@ import { emailUtil } from './email.util';
 import { SendVerificationCodeToEmail } from './interfaces/send-verification-code.util.interface';
 import { redisUtil } from './redis.util';
 
-export const verificationCodeToEmail = async (
-  sendInfo: SendVerificationCodeToEmail,
-): Promise<ResponseDto> => {
-  const { email, subject, contents, verificationCode, expireTime } = sendInfo;
+export const verificationCode = () => {
+  return {
+    sendToEmail: async (
+      sendInfo: SendVerificationCodeToEmail,
+    ): Promise<ResponseDto> => {
+      const { email, subject, contents, verificationCode, expireTime } =
+        sendInfo;
 
-  await redisUtil().setExpire({
-    key: email,
-    value: verificationCode,
-    time: expireTime,
-  });
-  await emailUtil().send({ email, subject, contents });
+      await redisUtil().setExpire({
+        key: email,
+        value: verificationCode,
+        time: expireTime,
+      });
+      await emailUtil().send({ email, subject, contents });
 
-  return ResponseDto.success();
+      return ResponseDto.success();
+    },
+  };
 };
