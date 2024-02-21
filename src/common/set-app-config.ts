b968f87d8182256c3f67e088c9551ef5b1e1c03d
kiyoung92 <go3go1go2@gmail.com>
2024-02-21 23:53:48 +0900

    refactor: app set config

---
+++ src/common/set-app-config.ts
@@ -0,0 +1,25 @@
+import {
+  ClassSerializerInterceptor,
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AllExceptionsFilter } from './filters/exception.filter';
+import * as cookieParser from 'cookie-parser';
+import { configUtil } from './utils/config.util';
+import { prismaUtil } from './utils/prisma.util';
+
+export const setAppConfig = async <T extends INestApplication>(
+  app: T,
+): Promise<void> => {
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalFilters(new AllExceptionsFilter());
+  app.use(cookieParser());
+  app.enableCors({
+    origin: [configUtil().getClient()],
+    credentials: true,
+  });
+  await prismaUtil().onModuleInit();
+  await app.listen(configUtil().getPort());
+};
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,9 @@
-import { NestFactory, Reflector } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
-import { AllExceptionsFilter } from './common/filters/exception.filter';
-import * as cookieParser from 'cookie-parser';
-import { prismaUtil } from './common/utils/prisma.util';
-import { configUtil } from './common/utils/config.util';
+import { setAppConfig } from './common/set-app-config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.useGlobalFilters(new AllExceptionsFilter());
-  app.use(cookieParser());
-  app.enableCors({
-    origin: [configUtil().getClient()],
-    credentials: true,
-  });
-  await prismaUtil().onModuleInit();
-  await app.listen(configUtil().getPort());
+  await setAppConfig(app);
 }
 bootstrap();
