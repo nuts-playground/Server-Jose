33260b46c22a364e1d126b103d890add91caa804
kiyoung92 <go3go1go2@gmail.com>
2024-02-23 23:43:00 +0900

    test: user controller
    컨트롤러 테스트 세팅

--- src/user/test/user.controller.spec.ts
+++ src/user/test/user.controller.spec.ts
@@ -1,1 +1,16 @@
-describe('UserController', () => {});
+import { UserController } from '../user.controller';
+import { Test } from '@nestjs/testing';
+
+describe('UserController', () => {
+  let userController: UserController;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      controllers: [UserController],
+    }).compile();
+
+    userController = module.get<UserController>(UserController);
+  });
+
+  describe('이메일 유효성 검사', () => {});
+});
