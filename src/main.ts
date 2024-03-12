fd80bb2a67cdf50733307936ba29e120c90e42ae
kiyoung92 <go3go1go2@gmail.com>
2024-03-13 00:57:29 +0900

    refactor: Global Env Config 환경변수 파일 리팩터링

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,34 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './common/filters/exception.filter';
+import * as cookieParser from 'cookie-parser';
+import { ConfigGlobal } from './global/config.global';
 import { AppGlobal } from './global/app.global';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await AppGlobal.beforeAll(app);
+  const config = new DocumentBuilder()
+    .setTitle('NestJS API')
+    .setDescription('NestJS API description')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalFilters(new AllExceptionsFilter());
+  app.use(cookieParser());
+  app.enableCors({
+    origin: [ConfigGlobal.env.clientUrl],
+    credentials: true,
+  });
+  await AppGlobal.prisma.$connect();
+  await app.listen(ConfigGlobal.env.port);
+
+  // await AppGlobal.beforeAll(app);
 }
 bootstrap();
